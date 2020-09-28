9d32772fc5100f9d80e1048714ed5025600afc0f
a111 <qwerty>
2020-09-29 00:19:12 +0300

    testing..

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const test = React.createElement('div', {
+    id: 'test-id',
+    className: 'test-class',
+    onClick: () => {
+        alert('hello')
+    }
+}, <div>Hello</div>);
+
+const onClick = () => {
+    alert('hello from CustomButton')
+};
+const btnId = 'btn-test-id';
+
+const CustomButton = (props) => {
+    console.log(props);
+    const {id, children} = props;
+
+    return (
+        <button onClick={onClick} id={id}>{children}</button>
+    );
+};
+
+
+const testBtn = React.createElement(CustomButton, {id: 'test-btn-2'}, 'Press-me');
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <CustomButton id={btnId}>srf afeaw</CustomButton>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
