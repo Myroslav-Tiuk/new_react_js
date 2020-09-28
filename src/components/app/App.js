3927c6315fbe3e02f70333c9c2b5e5934925d71e
a111 <qwerty>
2020-09-29 02:10:35 +0300

    testing..

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from "../header/Header";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            test
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
-const test = React.createElement('div', {
-    id: 'test-id',
-    className: 'test-class',
-    onClick: () => {
-        alert('hello')
-    }
-}, <div>Hello</div>);
-
-const onClick = () => {
-    alert('hello from CustomButton')
-};
-const btnId = 'btn-test-id';
-
-const CustomButton = (props) => {
-    console.log(props);
-    const {id, children} = props;
-
-    return (
-        <button onClick={onClick} id={id}>{children}</button>
-    );
-};
-
-
-const testBtn = React.createElement(CustomButton, {id: 'test-btn-2'}, 'Press-me');
 
 ReactDOM.render(
-    <CustomButton id={btnId}>srf afeaw</CustomButton>,
+    <App/>,
     document.getElementById('root')
 );
 
